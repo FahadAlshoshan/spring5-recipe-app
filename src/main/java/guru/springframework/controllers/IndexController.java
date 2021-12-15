288c72ffe942b75f554d969ecab1efbf3187f15c
FahadAlshoshan <Fahad.a.alshoshan@gmail.com>
2021-12-15 09:22:04 +0300

    commit

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -8,8 +8,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 public class IndexController {
+    @RequestMapping({"","/index","/"})
 
-    @RequestMapping({"", "/", "/index"})
     public String getIndexPage(){
         return "index";
     }
